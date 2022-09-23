01f201471b643ed567a91f8e2a24ed0c149f83bd
Olesia Barannik <112662408+OlesiaBarannik@users.noreply.github.com>
2022-09-23 15:18:10 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -2,20 +2,14 @@ def math():
     try:
         a = int(input("Please, enter two numbers A: "))
         b = int(input("Please, enter two numbers B: "))
+        result = a ** 2 / b
+        print(result)
+        
     except ValueError:
         print("Could not convert data to an integer.")
-        return False
-
-    try:
-        result = a ** 2 / b
     except ZeroDivisionError:
         print("You cannot divide by zero")
-        return False
 
-    print(result)
-    return result
+
 math()
-
-
-
 
